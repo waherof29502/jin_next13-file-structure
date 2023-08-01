7cda4b86be6709316b28fd029694ba2bedd312e1
jinwangdev <jinwangdev>
2023-08-01 15:27:52 +0800

    feat(api router): add api router demo

---
+++ src/app/api/todos/[id]/route.ts
@@ -0,0 +1,18 @@
+import { NextResponse } from 'next/server';
+
+const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
+const API_KEY: string = process.env.DATA_API_KEY as string;
+type Todo = {
+  userId: number;
+  id: number;
+  title: string;
+  completed: boolean;
+};
+
+export async function GET(request: Request) {
+  const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+  const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
+  const todo: Todo = await res.json();
+  if (!todo.id) return NextResponse.json({ message: 'Todo not found' });
+  return NextResponse.json(todo);
+}
