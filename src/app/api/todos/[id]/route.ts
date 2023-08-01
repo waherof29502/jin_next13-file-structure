cbb3cac06d7ba927c8747d2cc9ef54ecdc8b5ea9
jinwangdev <jinwangdev>
2023-08-01 17:09:36 +0800

    feat(middleware): add middleware

--- src/app/api/todos/[id]/route.ts
+++ src/app/api/todos/[id]/route.ts
@@ -2,6 +2,11 @@ import { NextResponse } from 'next/server';
 
 const DATA_SOURCE_URL = 'https://jsonplaceholder.typicode.com/todos';
 const API_KEY: string = process.env.DATA_API_KEY as string;
+type Props = {
+  params: {
+    id: string;
+  };
+};
 type Todo = {
   userId: number;
   id: number;
@@ -9,8 +14,8 @@ type Todo = {
   completed: boolean;
 };
 
-export async function GET(request: Request) {
-  const id = request.url.slice(request.url.lastIndexOf('/') + 1);
+export async function GET(request: Request, { params: { id } }: Props) {
+  //const id = request.url.slice(request.url.lastIndexOf('/') + 1);
   const res = await fetch(`${DATA_SOURCE_URL}/${id}`);
   const todo: Todo = await res.json();
   if (!todo.id) return NextResponse.json({ message: 'Todo not found' });
--- src/app/api/todos/route.ts
+++ src/app/api/todos/route.ts
@@ -9,10 +9,16 @@ type Todo = {
   completed: boolean;
 };
 
-export async function GET() {
+export async function GET(request: Request) {
+  const origin = request.headers.get('origin');
   const res = await fetch(DATA_SOURCE_URL);
   const todos: Todo[] = await res.json();
-  return NextResponse.json(todos);
+  return new NextResponse(JSON.stringify(todos), {
+    headers: {
+      'Access-Control-Allow-Origin': origin || '*',
+      'Content-Type': 'application/json'
+    }
+  });
 }
 
 export async function DELETE(request: Request) {
