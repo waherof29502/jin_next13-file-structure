66659f655630c355901618fd9a048ade7ecee8b8
jinwangdev <jinwangdev>
2023-08-02 17:22:27 +0800

    feat(calendar): add calendar second version

--- src/app/(lobby)/calendar/page.tsx
+++ src/app/(lobby)/calendar/page.tsx
@@ -1,8 +1,14 @@
 import Calendar from '@/components/Calendar/calendar';
+import Calendar2 from '@/components/Calendar/calendar2';
 import { FC } from 'react';
 
 const page: FC = () => {
-  return <Calendar />;
+  return (
+    <div className="flex flex-col">
+      <Calendar />
+      <Calendar2 />
+    </div>
+  );
 };
 
 export default page;
